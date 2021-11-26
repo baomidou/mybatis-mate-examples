60b4a9109ef5f1b142a72797e7c801dd23e5a9df
hubin <243194995@qq.com>
2021-11-26 18:20:42 +0800

    补全升级导致的缺失逻辑

--- mybatis-mate-sharding-jta-atomikos/src/main/java/mybatis/mate/sharding/jta/atomikos/config/ShardingConfig.java
+++ mybatis-mate-sharding-jta-atomikos/src/main/java/mybatis/mate/sharding/jta/atomikos/config/ShardingConfig.java
@@ -4,6 +4,9 @@ import com.mysql.cj.jdbc.MysqlXADataSource;
 import mybatis.mate.config.DataSourceProperty;
 import mybatis.mate.provider.AtomikosDataSourceProvider;
 import mybatis.mate.provider.IDataSourceProvider;
+import mybatis.mate.sharding.IShardingProcessor;
+import mybatis.mate.sharding.ShardingHelper;
+import org.apache.ibatis.mapping.MappedStatement;
 import org.postgresql.xa.PGXADataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -53,6 +56,20 @@ public class ShardingConfig {
                 return mysqlXaDataSource;
             }
         };
+    }
+
+    @Bean
+    public IShardingProcessor shardingProcessor() {
+        /**
+         * 切换数据源，返回 false 使用默认数据源切换规则
+         *
+         * @param args            执行参数
+         * @param mappedStatement {@link MappedStatement}
+         * @param datasourceKey   数据源关键字
+         * @return true 手动切换成功，底层不会处理  false 交给底层随机自行切换
+         */
+        // 改变系统切换策略可以实现 IShardingStrategy 策略
+        return (args, mappedStatement, datasourceKey) -> false;
     }
 }
 
